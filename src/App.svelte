744923aab87d80551013fcf8f4b3c5ce83f91ef7
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-02-09 21:26:51 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import qrcode from "qrcode";
+
+  let value = "https://github.com/zasdaym";
+  let imageURL = "";
+
+  $: {
+    qrcode
+      .toDataURL(value, {
+        width: 240,
+      })
+      .then((dataURL) => (imageURL = dataURL));
+  }
+</script>
+
+<main>
+  <input type="text" bind:value />
+  <br />
+  <img src={imageURL} alt={value} />
+</main>
+
+<style>
+  main {
+    text-align: center;
+  }
+</style>
