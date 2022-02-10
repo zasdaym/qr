212cd9b22a180d24af4a97fa79ada80d6c3e1ce8
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-02-10 10:21:07 +0700

    Add line separator between input and QR

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
 
   let value = "https://github.com/zasdaym";
   let imageURL = "";
+  let height = 240;
+  let width = 240;
 
   $: {
     qrcode
@@ -15,8 +17,8 @@
 
 <main>
   <input type="text" bind:value />
-  <br />
-  <img src={imageURL} alt={value} />
+  <hr />
+  <img src={imageURL} alt={value} {height} {width} />
 </main>
 
 <style>
